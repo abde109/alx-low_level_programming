259751ba89ad92da6312507ff837083d7b846ee0
abde109 <abderrahimkhadri2@gmail.com>
2023-03-24 01:11:21 +0000

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,16 +7,16 @@
  */
 int _isupper(int c)
 {
-    int i;
+	int i;
 
-    if (c >= 65 && c <= 90)
-    {
-        i = 1;
-    }
-    else
-    {
-        i = 0;
-    }
+	if (c >= 65 && c <= 90)
+	{
+		i = 1;
+	}
+	else
+	{
+		i = 0;
+	}
 
-    return (i);
+	return (i);
 }
