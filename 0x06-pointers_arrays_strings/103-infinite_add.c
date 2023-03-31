65e57fb3f1ea04c320497b2a5aab9e940f1ae5e2
abde109 <abderrahimkhadri2@gmail.com>
2023-03-31 15:01:10 +0000

    update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,56 +1,27 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * is_printable - checks if a character is printable
- * @c: character to check
- *
- * Return: 1 if printable, 0 otherwise
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ * Return: the resulting strring
  */
-int is_printable(char c)
+char *rot13(char *s)
 {
-    return (c >= 32 && c <= 126);
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+	    }
+	}
+	return (s);
 }
-
-/**
- * print_buffer - prints a buffer
- * @b: buffer to print
- * @size: size of the buffer
- */
-void print_buffer(char *b, int size)
-{
-    int i, j;
-
-    for (i = 0; i < size; i += 10)
-    {
-        printf("%08x: ", i);
-
-        for (j = 0; j < 10; j++)
-        {
-            if (i + j < size)
-                printf("%02x", b[i + j]);
-            else
-                printf("  ");
-
-            if (j % 2)
-                printf(" ");
-        }
-
-        for (j = 0; j < 10; j++)
-        {
-            if (i + j < size)
-            {
-                if (is_printable(b[i + j]))
-                    putchar(b[i + j]);
-                else
-                    putchar('.');
-            }
-        }
-
-        putchar('\n');
-    }
-
-    if (size <= 0)
-        putchar('\n');
-}
-
