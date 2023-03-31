1e785b3b198592ed9b4f151cfccc3cd96469554e
abde109 <abderrahimkhadri2@gmail.com>
2023-03-31 02:29:17 +0000

    update

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,101 +2,78 @@
 #include <stdio.h>
 
 /**
- * isPrintableASCII - determines if n is a printable ASCII char.
- * @n: integer
- *
- * Return: 1 if true, 0 if false.
+ * rev_array - reverse array
+ * @n: integer params
+ * Return: 0
  */
-int isPrintableASCII(int n)
+
+void rev_array(char *n)
 {
-	return (n >= 32 && n <= 126);
+    int i = 0;
+    int j = 0;
+    char temp;
+
+    while (*(n + i) != '\0')
+    {
+        i++;
+    }
+    i--;
+
+    for (j = 0; j < i; j++, i--)
+    {
+        temp = *(n + j);
+        *(n + j) = *(n + i);
+        *(n + i) = temp;
+    }
 }
 
 /**
- * printHexes - func that prints hex values for string b in formatted form.
- * @b: pointer to string to print.
- * @start: starting position.
- * @end: ending position.
- *
- * Return: No return.
- */
-void printHexes(char *b, int start, int end)
-{
-	int i = 0;
-
-	while (i < 10)
-	{
-		if (i < end)
-			printf("%02x", *(b + start + i));
-		else
-			printf("  ");
-		if (i % 2)
-			printf(" ");
-		i++;
-	}
-}
-
-/**
- * printASCII - function that prints ascii values for string b,
- * formatted to replace nonprintable chars with '.'
- * @b: pointer to string to print.
- * @start: starting position.
- * @end: ending position.
- *
- * Return: No return.
- */
-void printASCII(char *b, int start, int end)
-{
-	int ch, i = 0;
-
-	while (i < end)
-	{
-		ch = *(b + i + start);
-		if (!isPrintableASCII(ch))
-			ch = 46;
-		printf("%c", ch);
-		i++;
-	}
-}
-
-/**
- * print_buffer - function that prints content of size bytes buffer,
- * pointed by b.
- * @b: pointer to string.
- * @size: size of buffer.
- *
- * The output should print 10 bytes per line.
- * Each line starts with the position of the first byte of the line in,
- * hexadecimal (8 chars), starting with 0.
- * Each line shows the hexadecimal content (2 chars) of the buffer, 2 bytes,
- * at a time, separated by a space.
- * Each line shows the content of the buffer. If the byte is a printable,
- * character, print the letter, if not, print . (dot)
- * Each line ends with a new line \n.
- * If size is 0 or less, the output should be a new line only \n.
- * You are allowed to use the standard library.
- * The output should look like the example shown, and formatted exactly the,
- * same way
- *
- * Return: No return.
-
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
  */
 
-void print_buffer(char *b, int size)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int start, end;
+    int carry = 0, i = 0, j = 0, digits = 0;
+    int val1 = 0, val2 = 0, total = 0;
 
-	if (size > 0)
-	{
-		for (start = 0; start < size; start += 10)
-		{
-			end = (size - start < 10) ? size - start : 10;
-			printf("%08x: ", start);
-			printHexes(b, start, end);
-			printASCII(b, start, end);
-			printf("\n");
-		}
-	} else
-		printf("\n");
+    while (*(n1 + i) != '\0')
+        i++;
+    while (*(n2 + j) != '\0')
+        j++;
+    i--;
+    j--;
+    if (j >= size_r || i >= size_r)
+        return (0);
+    while (j >= 0 || i >= 0 || carry == 1)
+    {
+        if (i < 0)
+            val1 = 0;
+        else
+            val1 = *(n1 + i) - '0';
+        if (j < 0)
+            val2 = 0;
+        else
+            val2 = *(n2 + j) - '0';
+        total = val1 + val2 + carry;
+        if (total >= 10)
+            carry = 1;
+        else
+            carry = 0;
+        if (digits >= (size_r - 1))
+            return (0);
+        r[digits] = (total % 10) + '0';
+        digits++;
+        j--;
+        i--;
+    }
+    if (digits == size_r)
+        return (0);
+    r[digits] = '\0';
+    rev_array(r);
+    return (r);
 }
-
