2cd404c5a50258f090fb2df4ed0bbe9005f7ec29
abde109 <abderrahimkhadri2@gmail.com>
2023-04-05 19:56:21 -0700

    update

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+int wildcmp(char *str1, char *str2)
+{
+	if (*str1 == '\0')
+	{
+		if (*str2 != '\0' && *str2 == '*')
+		{
+			return (wildcmp(str1, str2 + 1));
+		}
+		return (*str2 == '\0');
+	}
+
+	if (*str2 == '*')
+	{
+		return (wildcmp(str1 + 1, str2) || wildcmp(str1, str2 + 1));
+	}
+	else if (*str1 == *str2)
+	{
+		return (wildcmp(str1 + 1, str2 + 1));
+	}
+	return (0);
+}
+
