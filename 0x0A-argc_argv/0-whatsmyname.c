4f0cbc39cc85f5d9fcbd67eff3084b98d3504b55
abde109 <abderrahimkhadri2@gmail.com>
2023-04-10 19:25:49 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the name of the program
+ * @argc: The number of command-line arguments
+ * @argv: The array of command-line arguments
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+    if (argc > 0)
+    {
+        printf("%s\n", argv[0]);
+    }
+    return (0);
+}
+
