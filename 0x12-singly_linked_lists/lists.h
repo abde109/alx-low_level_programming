61ee49adcdcb49a3c4d4a7d47325818fce72916d
abde109 <abderrahimkhadri2@gmail.com>
2023-04-27 18:10:03 -0700

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,23 @@
+/* File: lists.h */
+
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
 #include <string.h>
 
+/* Structure for list_t */
 typedef struct list_s
 {
     char *str;
     unsigned int len;
     struct list_s *next;
 } list_t;
+
+/* Other function prototypes... */
+
+/* Prototype for print_list */
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
 
