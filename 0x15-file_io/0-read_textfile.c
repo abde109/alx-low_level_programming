5d2f577cbaba8e8ee421883adff96ea5e9f9dc24
abde109 <abderrahimkhadri2@gmail.com>
2023-05-08 16:26:07 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,9 +10,8 @@
  * @filename: The name of the file
  * @letters: The number of letters it should read and print
  *
- * Return: The actual number of letters it could read and print, or 0 on failure
+ * Return:The actual number of letters it could read and print, or 0 on failure
  */
- 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
