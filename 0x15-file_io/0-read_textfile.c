a6e1cf0083f7cb27e862b8373630f6c53ec6285c
abde109 <abderrahimkhadri2@gmail.com>
2023-05-08 16:23:05 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,45 +12,47 @@
  *
  * Return: The actual number of letters it could read and print, or 0 on failure
  */
+ 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t bytes_read, bytes_written;
-    char *buffer;
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    bytes_read = read(fd, buffer, letters);
-    if (bytes_read == -1)
-    {
-        close(fd);
-        free(buffer);
-        return (0);
-    }
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
 
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        close(fd);
-        free(buffer);
-        return (0);
-    }
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
 
-    close(fd);
-    free(buffer);
+	close(fd);
+	free(buffer);
 
-    return (bytes_written);
+	return (bytes_written);
 }
+
 
