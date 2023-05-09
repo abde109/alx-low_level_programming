6b8514e6d1bdd99e5d877995ffd4d7dcf57b77c0
abde109 <abderrahimkhadri2@gmail.com>
2023-05-08 17:00:24 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+int copy_file(const char *file_from, const char *file_to);
+
 /**
  * main - Entry point
  * @argc: The number of command-line arguments
@@ -14,61 +16,50 @@
  */
 int main(int argc, char *argv[])
 {
-    int fd_from, fd_to, read_bytes, write_bytes;
-    char buffer[1024];
-
     if (argc != 3)
     {
         dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
         exit(97);
     }
 
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+    return copy_file(argv[1], argv[2]);
+}
 
-    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+/**
+ * copy_file - Copies the content of one file to another file
+ * @file_from: The source file
+ * @file_to: The destination file
+ *
+ * Return: 0 on success, otherwise a positive error code
+ */
+int copy_file(const char *file_from, const char *file_to)
+{
+    int fd_from, fd_to, read_bytes, write_bytes;
+    char buffer[1024];
+
+    fd_from = open(file_from, O_RDONLY);
+    if (fd_from == -1)
+        return (dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from), close(fd_from), 98);
+
+    fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
     if (fd_to == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        close(fd_from);
-        exit(99);
-    }
+        return (dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), close(fd_from), 99);
 
     while ((read_bytes = read(fd_from, buffer, 1024)) > 0)
     {
         write_bytes = write(fd_to, buffer, read_bytes);
         if (write_bytes != read_bytes)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-            close(fd_from);
-            close(fd_to);
-            exit(99);
-        }
+            return (dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), close(fd_from), close(fd_to), 99);
     }
 
     if (read_bytes == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        close(fd_from);
-        close(fd_to);
-        exit(98);
-    }
+        return (dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from), close(fd_from), close(fd_to), 98);
 
     if (close(fd_from) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-        exit(100);
-    }
+        return (dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), 100);
 
     if (close(fd_to) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-        exit(100);
-    }
+        return (dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), 100);
 
     return (0);
 }
---
+++ 0x15-file_io/ubuntu64.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, Ubuntu 64-bit!\n");
+    return 0;
+}
+
